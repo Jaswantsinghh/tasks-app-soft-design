f95402ebb6f86b981105626a074bd4cd26e238eb
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-08-10 13:16:04 +0530

    datebar added

--- src/modules/mainPage.jsx
+++ src/modules/mainPage.jsx
@@ -4,13 +4,24 @@ export default function MainPage()
 {
     return(
         <>
-        <h1>Done</h1>
+        <div className="mainContainer">
+            <h1 className="mainHeader">My tasks</h1>
+            <div className="mainDates">
+              <span className="date">MON 12</span>
+              <span className="date">TUE 13</span>
+              <span className="date">WED 14</span>
+              <span className="dateSelected">THU 15</span>
+              <span className="date">FRI 16</span>
+              <span className="date">SAT 17</span>
+              <span className="date">SUN 18</span>
+            </div>
         <div className="navWrapper">
 <p>NavBar</p>
 <p>NavBar</p>
 <p>NavBar</p>
 <p>NavBar</p>
 <p>NavBar</p>
+        </div>
         </div>
         </>
     );
