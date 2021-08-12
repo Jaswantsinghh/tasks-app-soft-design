04cdc97fdb26babcf0fd53e0631316e1f83b12e0
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-08-12 21:39:35 +0530

    dark mode added

--- src/modules/mainPage.jsx
+++ src/modules/mainPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import '../index.scss';
 import home from '../img/icons/home-page.png';
 import hamburger from '../img/icons/square.png';
@@ -7,25 +7,26 @@ import setting from '../img/icons/settings.png';
 import user from '../img/icons/user.png';
 import pen from '../img/icons/pencil.png';
 import dustbin from '../img/icons/dustbin.png';
+import penWhite from '../img/icons/pencil-white.png';
 
 function Card(props)
 {
     return(
         <>
-        <div className="cardFlex">
+        <div className={props.dark ? "cardDark" : "cardFlex"}>
         <div className="cards">
             <div className="header">
-            <h1 className="titleCard">{props.title}</h1>
+            <h1 className={props.dark ? "titleDark" : "titleCard"}>{props.title}</h1>
             <div className={props.color}></div>
             <span className="titleTime">Today 15:04</span>
             </div>
-            <p className="cardTask">{props.one}</p>
-            <p className="cardTask">{props.two}</p>
-            <p className="cardTask">{props.three}</p>
+            <p className={props.dark ? "taskDark" : "cardTask"}>{props.one}</p>
+            <p className={props.dark ? "taskDark" : "cardTask"}>{props.two}</p>
+            <p className={props.dark ? "taskDark" : "cardTask"}>{props.three}</p>
         </div>
         <div className="actionButtons">
-<a><img alt="edit"className="penIcon" src={pen}></img></a>
-<div className="delete"><img className="delIcon"alt="delete" src={dustbin}></img></div>
+<a><img alt="edit"className="penIcon" src={props.dark ? penWhite : pen}></img></a>
+<div className={props.dark ? "delDark" : "delete"}><img className="delIcon"alt="delete" src={dustbin}></img></div>
         </div>
         </div>
         </>
@@ -34,10 +35,15 @@ function Card(props)
 
 export default function MainPage()
 {
+    const [isDark, setDark] = useState(false);
+    const toggle = (e)=> { 
+        e.preventDefault();
+        setDark(!isDark);
+    }
     return(
         <>
-        <div className="main">
-        <div className="mainContainer">
+        <div className={isDark ? "mainDark" : "main"}>
+        <div className={isDark ? "mainContDark" : "mainContainer"}>
             <h1 className="mainHeader">My tasks</h1>
             <div className="mainDates">
               <span className="date">MON 12</span>
@@ -48,19 +54,19 @@ export default function MainPage()
               <span className="date">SAT 17</span>
               <span className="date">SUN 18</span>
             </div>
-            <div className="cardHolder">
-<Card title="Work" color="blue" one="Respond emails" two="Call Angela" three="Meeting with clients" />
-<Card title="Home" color="green" one="Pay laundry" two="Doing Laundry" three="Cleaning Bathrooms" />
-<Card title="Pets" color="yellow" one="Collar" two="Shampoo" three="Food" />
-<Card title="Work" color="red" one="Respond emails" two="Call Angela" three="Meeting with clients" />
-<Card title="Work" color="yellow" one="Respond emails" two="Call Angela" three="Meeting with clients" />
+            <div className={isDark ? "cardHolderDark" : "cardHolder"}>
+<Card title="Work" dark={isDark} color="blue" one="Respond emails" two="Call Angela" three="Meeting with clients" />
+<Card title="Home" dark={isDark} color="green" one="Pay laundry" two="Doing Laundry" three="Cleaning Bathrooms" />
+<Card title="Pets" dark={isDark} color="yellow" one="Collar" two="Shampoo" three="Food" />
+<Card title="Work" dark={isDark} color="red" one="Respond emails" two="Call Angela" three="Meeting with clients" />
+<Card title="Work" dark={isDark} color="yellow" one="Respond emails" two="Call Angela" three="Meeting with clients" />
             </div>
         </div>
         <div className="navWrapper">
 <a className="anc"><img className="navIcons" src={home} alt="home"></img></a>
 <a className="anc"><img className="navIcons" src={hamburger} alt="home"></img></a>
-<a className="ancMain"><img className="navIcons" src={plus} alt="home"></img></a>
-<a className="anc"><img className="navIcons" src={setting} alt="home"></img></a>
+<a className={isDark ? "ancDark" : "ancMain"}><img className="navIcons" src={plus} alt="home"></img></a>
+<a onClick={toggle} className="anc"><img className="navIcons" src={setting} alt="home"></img></a>
 <a className="anc"><img className="navIcons" src={user} alt="home"></img></a>
         </div>
         </div>
